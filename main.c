0dd0e09bc3c0b0c3dfb8b3ea254ff5f16fc30013
AnasAPPR <anasappraoui15@gmail.com>
2025-05-11 19:54:26 +0200

    main.c

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include "cy_fighters.h"
 
-// Fonction principale du jeu
-int main() {
-    srand(time(NULL)); // Initialisation aléatoire
+
+int main() {                  // Main function of the game
+    srand(time(NULL)); 
 
     printf("========================================\n");
     printf("           \U0001F4A5 CY-FIGHTERS \U0001F4A5\n");
@@ -15,7 +15,7 @@ int main() {
         if (num_players != 1 && num_players != 2) {
             printf("Invalid input. Please enter 1 or 2.\n");
         }
-    } while (num_players != 1 && num_players != 2);
+    } while (num_players != 1 && num_players != 2);        //Choice of the number of players
 
     int team_size;
     do {
@@ -24,19 +24,19 @@ int main() {
         if (team_size < 1 || team_size > MAX_COMBATTANTS) {
             printf("Invalid team size. Please enter a number between 1 and %d.\n", MAX_COMBATTANTS);
         }
-    } while (team_size < 1 || team_size > MAX_COMBATTANTS);
+    } while (team_size < 1 || team_size > MAX_COMBATTANTS);    //Choice of the number of fighters
 
     Team team1, team2;
-    configure_team(&team1, team_size, "Player 1");
+    configure_team(&team1, team_size, "Player 1");            //Choice of fighters for team 1
 
     int ai_control = 0;
     if (num_players == 2) {
-        configure_team(&team2, team_size, "Player 2");
+        configure_team(&team2, team_size, "Player 2");        //Choice of fighters for team 2
     } else {
         strcpy(team2.name, "AI");
         team2.member_count = team_size;
         for (int i = 0; i < team_size; i++) {
-            team2.members[i] = available_fighters[rand() % total_fighters];
+            team2.members[i] = available_fighters[rand() % total_fighters];    //Choice of fighters for AI team 
         }
         ai_control = 1;
     }
@@ -50,11 +50,11 @@ int main() {
         if (is_team_KO(team2)) break;
         printf("\n--- Turn of team %s ---\n", team2.name);
         team_turn(&team2, &team1, ai_control);
-    }
+    }                                                    //functions to make the fight
 
     printf("\n=== End of Battle ===\n");
     if (is_team_KO(team1)) printf("%s wins!\n", team2.name);
-    else printf("%s wins!\n", team1.name);
+    else printf("%s wins!\n", team1.name);              //end of the figth 
 
     return 0;
 }
