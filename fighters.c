2a3fb5896ec3eba5030791401c59c9884885b9fd
AnasAPPR <anasappraoui15@gmail.com>
2025-05-11 21:38:11 +0200

    fighters.c

--- fighters.c
+++ fighters.c
@@ -2,12 +2,12 @@
 
 // Definition of available combatants
 Fighter available_fighters[] = {
-    {"Yassa", 100, 100, 30, 10, 2, { {"Brava - 40 damages", "attack", 40, 2, 0}, {"heal - 20", "heal", 20, 3, 0} } },
-    {"Barim", 150, 150, 20, 25, 4, { {"highkick - 30 damages", "attack", 30, 2, 0}, {"heal - 30", "heal", 30, 3, 0} } },
-    {"Mit", 120, 120, 25, 20, 4, { {"Flip - 35 damages", "attack", 35, 2, 0}, {"heal - 25", "heal", 25, 3, 0} } },
-    {"Sana", 90, 90, 20, 15, 10, { {"head crushing - 55 damages", "attack", 55, 2, 0}, {"heal - 40", "heal", 40, 3, 0} } },
-    {"Juwaw", 110, 110, 35, 15, 1, { {"musty - 40 damages", "attack", 40, 2, 0}, {"heal - 25", "heal", 25, 3, 0} } },
-    {"resaf", 90, 90, 15, 15, 3, { {"smock - 35 damages", "attack", 35, 2, 0}, {"heal - 25", "heal", 25, 3, 0} } },
+    {"Yassa", 100, 100, 30, 10, 20, { {"Brava - 40 damages", "attack", 40, 2, 0}, {"heal - 20", "heal", 20, 3, 0} } },
+    {"Barim", 150, 150, 20, 25, 40, { {"highkick - 30 damages", "attack", 30, 2, 0}, {"heal - 30", "heal", 30, 3, 0} } },
+    {"Mit", 120, 120, 25, 20, 40, { {"Flip - 35 damages", "attack", 35, 2, 0}, {"heal - 25", "heal", 25, 3, 0} } },
+    {"Sana", 90, 90, 20, 15, 100, { {"head crushing - 55 damages", "attack", 55, 2, 0}, {"heal - 40", "heal", 40, 3, 0} } },
+    {"Juwaw", 110, 110, 35, 15, 10, { {"musty - 40 damages", "attack", 40, 2, 0}, {"heal - 25", "heal", 25, 3, 0} } },
+    {"resaf", 90, 90, 15, 15, 30, { {"smock - 35 damages", "attack", 35, 2, 0}, {"heal - 25", "heal", 25, 3, 0} } },
 };
 
 // Calculation of the total number of combatants
